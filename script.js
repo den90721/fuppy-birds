d392b6c7ab40ef5655a38749101c332b15c5d60b
den90721 <dgogunokov@gmail.com>
2024-09-05 23:37:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ const overImg = new Image();
 overImg.src = 'assets/OVER.svg';
 
 const puskImg = new Image();
-puskImg.src = 'assets/pusk.png'; // Обновлено на pusk.png
+puskImg.src = 'assets/pusk.svg'; // Обновлено на pusk.png
 
 const zastavkaImg = new Image();
 zastavkaImg.src = 'assets/zastavka (2).png';
